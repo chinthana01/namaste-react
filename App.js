fa5abc45945d6ea80784cec66854cfb02170ed3d
chinthana7 <chinthanamysore17@gmail.com>
2023-04-23 00:00:25 +0530

    first

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading=React.createElement("div",{id:"heading",xyz:"abcuuuuuuu"},React.createElement("h1",{id:"heading",xyz:"abcuuuuuuu"},"Im an h1 tag"));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
